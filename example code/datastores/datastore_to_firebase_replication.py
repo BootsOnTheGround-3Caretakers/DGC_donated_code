c0ee3c705ddfe5ede438495357fc6e0735143d19
Kevin Stacey <kevin@nova-wd.com>
2020-03-26 00:40:16 -0500

    added missing example

--- example code/datastores/datastore_to_firebase_replication.py
+++ example code/datastores/datastore_to_firebase_replication.py
@@ -139,7 +139,260 @@ class ReplicateToFirebase(object):
             return {'success': False,'return_msg':return_msg,'debug_data':debug_data}
         
         return {'success': True,'return_msg':return_msg,'debug_data':debug_data}
-         
+
+   
+    def __DatastoreUser(self,entity_id,entity,delete_flag = False):
+        return_msg = "ReplicateToFirebase:__DatastoreUser "
+        debug_data = []
+        call_result = {}
+        firebase_fields = []
+       
+
+        debug_data_count = 0
+        generated_fields =[]
+        
+    ##we need to get all the values in the record we are updating so we can put all needed info in firebase
+        call_result = entity.kget(entity.key)
+        if call_result['success'] != True:
+            return_msg += "get of object attribute record failed"
+            return {'success': False,'return_msg':return_msg,'debug_data':debug_data,'firebase_fields':firebase_fields}
+        
+        #ndb.Model overloads the = operator so we aren't overwriting the memory location of our current function
+        self = call_result['get_result']
+    ##</end>we need to get all the values in the record we are updating so we can put all needed info in firebase
+    
+    ## get all those values into a dictionary for easier access
+        call_result = self.getDict(self,["firebase_uid","user_uid","user_web_uid","first_name",'last_name','contact_email'])
+        debug_data.append(call_result)
+        if call_result['success'] != True:
+            return_msg += "failed to get entity fields dictionary"
+            return {'success': False,'return_msg':return_msg,'debug_data':debug_data,'firebase_fields':firebase_fields}
+
+        entity_data = call_result['dictionary']
+    ##</end> get all those values into a dictionary for easier access
+        
+        
+        try:
+            user_uid = entity_id.split("_",1)[1]
+        except Exception as e:
+            return_msg += "failed to parse user_uid from entity id:%s with exception:%s" % (entity_id,e)
+            return {'success': RC.input_validation_failed,'return_msg':return_msg,'debug_data':debug_data,'firebase_fields':firebase_fields}
+        
+     
+        
+        firebase_location = "user/" + entity_data['firebase_uid']
+    
+    ## user web UID
+        firebase_entry = FF()
+        call_result = firebase_entry.setFieldValues(firebase_location + "/meta_data/",
+                                                  FF.object_types.object,
+                                                  FF.functions.update,
+                                                  entity_data['contact_email'],
+                                                   FF.keys.user_contact_email)
+        debug_data.append(call_result)
+            
+        call_result = firebase_entry.toDict()
+        debug_data.append(call_result)
+        generated_fields.append(call_result['field'])
+        debug_data_count = debug_data_count +2
+    ##</end> user web UID
+    
+    ## user web UID
+        firebase_entry = FF()
+        call_result = firebase_entry.setFieldValues(firebase_location + "/meta_data/",
+                                                  FF.object_types.object,
+                                                  FF.functions.update,
+                                                  entity_data['user_web_uid'],
+                                                   FF.keys.user_web_uid)
+        debug_data.append(call_result)
+            
+        call_result = firebase_entry.toDict()
+        debug_data.append(call_result)
+        generated_fields.append(call_result['field'])
+        debug_data_count = debug_data_count +2
+    ##</end> user web UID
+    
+    ## user internal UID
+        firebase_entry = FF()
+        call_result = firebase_entry.setFieldValues(firebase_location + "/meta_data/",
+                                                  FF.object_types.object,
+                                                  FF.functions.update,
+                                                  unicode(entity_data['user_uid']),
+                                                   FF.keys.user_uid)
+        debug_data.append(call_result)
+            
+        call_result = firebase_entry.toDict()
+        debug_data.append(call_result)
+        generated_fields.append(call_result['field'])
+        debug_data_count = debug_data_count +2
+    ##</end> user internal UID
+    
+    
+    ## user first name
+        firebase_entry = FF()
+        call_result = firebase_entry.setFieldValues(firebase_location + "/meta_data/",
+                                                  FF.object_types.object,
+                                                  FF.functions.update,
+                                                  entity_data['first_name'],
+                                                   FF.keys.user_first_name)
+        debug_data.append(call_result)
+            
+        call_result = firebase_entry.toDict()
+        debug_data.append(call_result)
+        generated_fields.append(call_result['field'])
+        debug_data_count = debug_data_count +2
+    ##</end> user first name
+    
+    ## user last name
+        firebase_entry = FF()
+        call_result = firebase_entry.setFieldValues(firebase_location + "/meta_data/",
+                                                  FF.object_types.object,
+                                                  FF.functions.update,
+                                                  entity_data['last_name'],
+                                                   FF.keys.user_last_name)
+        debug_data.append(call_result)
+            
+        call_result = firebase_entry.toDict()
+        debug_data.append(call_result)
+        generated_fields.append(call_result['field'])
+        debug_data_count = debug_data_count +2
+    ##</end> user last name
+    
+    
+    
+    ## create user firebase_entry delete prevention key 
+        firebase_entry = FF()
+        call_result = firebase_entry.setFieldValues(firebase_location + "/dog_house/",
+                                                  FF.object_types.object,
+                                                  FF.functions.update,
+                                                  "deletion_prevention_key",
+                                                   FF.keys.deletion_prevention_key)
+        debug_data.append(call_result)
+            
+        call_result = firebase_entry.toDict()
+        debug_data.append(call_result)
+        generated_fields.append(call_result['field'])
+        debug_data_count = debug_data_count +2
+    ##</end> create user firebase_entry delete prevention key
+    
+    ## create web ui notifications last_updated key
+        firebase_entry = FF()
+        call_result = firebase_entry.setFieldValues(firebase_location + "/webui_notification_update/",
+                                                  FF.object_types.object,
+                                                  FF.functions.update,
+                                                  "1",
+                                                   FF.keys.last_updated)
+        debug_data.append(call_result)
+            
+        call_result = firebase_entry.toDict()
+        debug_data.append(call_result)
+        generated_fields.append(call_result['field'])
+        debug_data_count = debug_data_count +2
+    ##</end> create web ui notifications last_updated key
+    
+    ## create user object attribute rule joins updated notification
+        rule_join = FF()
+        call_result = rule_join.setFieldValues(firebase_location + "/updated_user_notifications/",
+                                                  FF.object_types.object,
+                                                  FF.functions.update,
+                                                  unicode(int(time.time())),
+                                                   FF.keys.last_updated)
+        debug_data.append(call_result)
+            
+        call_result = rule_join.toDict()
+        debug_data.append(call_result)
+        generated_fields.append(call_result['field'])
+        debug_data_count = debug_data_count +2
+    ##</end> create user object attribute rule joins updated notification
+    
+    
+    ## create user object attribute rule updated notification
+        rule = FF()
+        call_result = rule.setFieldValues(firebase_location + "/updated_user_rules/",
+                                                  FF.object_types.object,
+                                                  FF.functions.update,
+                                                  unicode(int(time.time())),
+                                                   FF.keys.last_updated)
+        debug_data.append(call_result)
+            
+        call_result = rule.toDict()
+        debug_data.append(call_result)
+        generated_fields.append(call_result['field'])
+        debug_data_count = debug_data_count +2
+    ##</end> create user object attribute rule updated notification
+    
+    ## create user payout change updated notification
+        rule = FF()
+        call_result = rule.setFieldValues(firebase_location + "/payout_change/",
+                                                  FF.object_types.object,
+                                                  FF.functions.update,
+                                                  unicode(int(time.time())),
+                                                   FF.keys.last_updated)
+        debug_data.append(call_result)
+            
+        call_result = rule.toDict()
+        debug_data.append(call_result)
+        generated_fields.append(call_result['field'])
+        debug_data_count = debug_data_count +2
+    ##</end> create user payout change updated notification
+    
+    ## create user billing change updated notification
+        rule = FF()
+        call_result = rule.setFieldValues(firebase_location + "/billing_change/",
+                                                  FF.object_types.object,
+                                                  FF.functions.update,
+                                                  unicode(int(time.time())),
+                                                   FF.keys.last_updated)
+        debug_data.append(call_result)
+            
+        call_result = rule.toDict()
+        debug_data.append(call_result)
+        generated_fields.append(call_result['field'])
+        debug_data_count = debug_data_count +2
+    ##</end> create user billing change updated notification
+    
+    ## create user notificaiton rules delete prevention key 
+        firebase_entry = FF()
+        call_result = firebase_entry.setFieldValues(firebase_location + "/notification_rules/",
+                                                  FF.object_types.object,
+                                                  FF.functions.update,
+                                                  "deletion_prevention_key",
+                                                   FF.keys.deletion_prevention_key)
+        debug_data.append(call_result)
+            
+        call_result = firebase_entry.toDict()
+        debug_data.append(call_result)
+        generated_fields.append(call_result['field'])
+        debug_data_count = debug_data_count +2
+    ##</end> create user notificaiton rules delete prevention key
+    
+    ## create user notification rule joins delete prevention key 
+        firebase_entry = FF()
+        call_result = firebase_entry.setFieldValues(firebase_location + "/notification_rule_joins/",
+                                                  FF.object_types.object,
+                                                  FF.functions.update,
+                                                  "deletion_prevention_key",
+                                                   FF.keys.deletion_prevention_key)
+        debug_data.append(call_result)
+            
+        call_result = firebase_entry.toDict()
+        debug_data.append(call_result)
+        generated_fields.append(call_result['field'])
+        debug_data_count = debug_data_count +2
+    ##</end> create user notification rule joins delete prevention key
+        
+        debug_data_count = debug_data_count * -1 
+        for data in debug_data[debug_data_count:]:
+            if data['success'] != True:
+                return_msg += "setting user_record or type record failed"
+                return {'success': False,'return_msg':return_msg,'debug_data':debug_data,'firebase_fields':firebase_fields}
+        
+        
+        firebase_fields = generated_fields
+        return {'success': True,'return_msg':return_msg,'debug_data':debug_data,'firebase_fields':firebase_fields}
+
+
+
     def __DsP1UserObjectAttributeRule(self, entity_id, entity, delete_flag=False):
         return_msg = "ReplicateToFirebase:__DsP1UserObjectAttributeRule "
         debug_data = []
@@ -307,6 +560,29 @@ class ReplicateToFirebase(object):
         firebase_fields = generated_fields
         return {'success': True, 'return_msg': return_msg, 'debug_data': debug_data, 'firebase_fields': firebase_fields}
 
+
+class DatastoreUser(ndb.Model,DSF,ReplicateToDatastore,ReplicateToDatastoreFlag,ReplicateToFirebaseFlag,ReplicateToFirebase):
+    #key user_uid
+    user_uid= ndb.IntegerProperty(required=True)
+    _rule_user_uid = [True,long,"bigint","greater0"]
+    user_web_uid = ndb.StringProperty(required=True)
+    _rule_user_web_uid = [True,unicode,"len1","user_web_uid"]
+    first_name = ndb.StringProperty(required=True)
+    _rule_first_name = [True,unicode,"len1","person_name"]
+    last_name = ndb.StringProperty(required=True)
+    _rule_last_name = [True,unicode,"len1","person_name"]
+    contact_email = ndb.StringProperty(required=True)
+    _rule_contact_email = [True,unicode,"len1","email_address"]
+    google_account_name = ndb.StringProperty(required=True)
+    _rule_google_account_name = [True,unicode,"len1","google_account_name"]
+    account_type = ndb.IntegerProperty(required=True)
+    _rule_account_type = [True,long,"account_type"]
+    creation_date = ndb.DateTimeProperty(required=False,auto_now_add=True)
+    _rule_creation_date = [False,datetime]
+    firebase_uid = ndb.StringProperty(required=True)
+    _rule_firebase_uid = [True,unicode,"len1"]
+    
+    
 
 class DatastoreUser(ndb.Model,DSF,ReplicateToFirebaseFlag,ReplicateToFirebase):
     #key user_uid
